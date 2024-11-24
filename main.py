0e9b23c7a3ffd74eb3aca6bd348a7080c64c03e8
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-11-24 20:00:57 +0530

    vehical

---
+++ main.py
@@ -0,0 +1,25 @@
+def move(self):
+class car(vehical):
+def move(self):
+print("I can role my whells")
+class bus(vehical):
+def move(self):
+print("I can travel")
+class airplane(vehical):
+def move(self):
+print("I can fly")
+class ship(vehical):
+def move(self):
+print("I can float")
+
+R = car ()
+R.move()
+
+K = bus()
+K.move()
+
+R = airplane()
+R.move()
+
+K = ship()
+K.move()
